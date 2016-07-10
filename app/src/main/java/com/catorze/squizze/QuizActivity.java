11b00d97921f8966fe3665ac9681f57e2e4871f5
Vitor Souza <contatovitordesouza@gmail.com>
2016-07-10 16:15:43 -0300

    Layouts sessao 1

--- app/src/main/java/com/catorze/squizze/QuizActivity.java
+++ app/src/main/java/com/catorze/squizze/QuizActivity.java
@@ -3,11 +3,11 @@ package com.catorze.squizze;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Login extends AppCompatActivity {
+public class QuizActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_quiz);
     }
 }
