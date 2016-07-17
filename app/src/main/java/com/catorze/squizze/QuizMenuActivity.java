1555c3a95894fa0f642d1213a9fa6e28000340f9
Alexandre Dias <Alexandre Dias>
2016-07-17 15:17:49 -0300

    Arquivos renomeados.!

--- app/src/main/java/com/catorze/squizze/QuizMenuActivity.java
+++ app/src/main/java/com/catorze/squizze/QuizMenuActivity.java
@@ -3,11 +3,11 @@ package com.catorze.squizze;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class DashboardActivity extends AppCompatActivity {
+public class QuizMenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+        setContentView(R.layout.activity_quiz_menu);
     }
 }
